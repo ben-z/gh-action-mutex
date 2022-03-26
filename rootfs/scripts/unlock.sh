9c134661a8a2633e1fa92ecf827bcfb737036399
Ben Zhang <benzhangniu@gmail.com>
2022-03-26 16:37:56 -0400

    Fix print

--- rootfs/scripts/unlock.sh
+++ rootfs/scripts/unlock.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source "$SCRIPT_DIR/utils.sh"
 
-echo "STATE_ticket_id"
+echo "$STATE_ticket_id"
 
 echo "Lock successfully acquired"
 
