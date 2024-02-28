7515ce5229d2e74b163d93510ec14859ebd964de
Fran R <regnoultf@gmail.com>
2024-02-28 17:43:34 +0000

    Fix mutex (#1)
    * Issues: 21 + 20

--- rootfs/scripts/utils.sh
+++ rootfs/scripts/utils.sh
@@ -20,7 +20,9 @@ update_branch() {
 	git switch --orphan gh-action-mutex/temp-branch-$(date +%s) --quiet
 	git branch -D $__branch --quiet 2>/dev/null || true
 	git fetch origin $__branch --quiet 2>/dev/null || true
-	git checkout $__branch --quiet || git switch --orphan $__branch --quiet
+	if ! git checkout origin/$__branch --quiet; then
+		git switch --orphan $__branch --quiet
+	fi
 }
 
 # Add to the queue
@@ -42,7 +44,7 @@ enqueue() {
 	touch $__queue_file
 
 	# if we are not in the queue, add ourself to the queue
-	if [ -z "$(cat $__queue_file | grep -F $__ticket_id)" ]; then
+	if ! grep -qx "$__ticket_id" "$__queue_file" ; then
 		echo "$__ticket_id" >> "$__queue_file"
 
 		git add $__queue_file
@@ -70,17 +72,20 @@ wait_for_lock() {
 	__queue_file=$2
 	__ticket_id=$3
 
-	echo "[$__ticket_id] Waiting for lock"
-
 	update_branch $__branch
 
 	# if we are not the first in line, spin
-	if [ "$(cat $__queue_file | awk NF | head -n 1)" != "$__ticket_id" ]; then
-		sleep 5
-		wait_for_lock $@
+	if [ -s $__queue_file ]; then
+		cur_lock=$(head -n 1 $__queue_file)
+		if [ "$cur_lock" != "$__ticket_id" ]; then
+			echo "[$__ticket_id] Waiting for lock - Current lock assigned to [$cur_lock]"
+			sleep 5
+			wait_for_lock $@
+		fi
+	else
+		echo "[$__ticket_id] $__queue_file unexpectedly empty, continuing"
 	fi
 }
-
 # Remove from the queue, when locked by it or just enqueued
 # args:
 #   $1: branch
@@ -95,22 +100,21 @@ dequeue() {
 
 	update_branch $__branch
 
-	if [ "$(cat $__queue_file | awk NF | head -n 1)" == "$__ticket_id" ]; then
+	if [[ "$(head -n 1 $__queue_file)" == "$__ticket_id" ]]; then
 		echo "[$__ticket_id] Unlocking"
 		__message="[$__ticket_id] Unlock"
-	else
+		# Remove top line
+		sed -i '1d' "$__queue_file"
+	elif grep -qx "$__ticket_id" "$__queue_file" ; then
 		echo "[$__ticket_id] Dequeueing. We don't have the lock!"
 		__message="[$__ticket_id] Dequeue"
-	fi
-
-	if [ $(awk "/$__ticket_id/" $__queue_file | wc -l) == "0" ]; then
+		# Remove the matching line
+		sed -i "/^${__ticket_id}$/d" $__queue_file
+	else
 		1>&2 echo "[$__ticket_id] Not in queue! Mutex file:"
 		cat $__queue_file
 		exit 1
 	fi
-
-	awk "!/$__ticket_id/" $__queue_file | awk NF > ${__queue_file}.new
-	mv ${__queue_file}.new $__queue_file
 
 	git add $__queue_file
 	git commit -m "$__message" --quiet
