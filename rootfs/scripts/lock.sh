0cb3d53a55081fb2a4ff47d3e410dafdf556c756
Zach Goldberg <zach@zachgoldberg.com>
2024-04-01 12:27:49 -0700

    bugfix attempt

--- rootfs/scripts/lock.sh
+++ rootfs/scripts/lock.sh
@@ -7,7 +7,7 @@ fi
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ $ARG_ACTION = "unlock" ]; then
-	bash "$SCRIPT_DIR/unlock.sh"
+	source "$SCRIPT_DIR/unlock.sh"
 	exit
 fi
 
