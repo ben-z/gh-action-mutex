79bc4ff549a0688920dbf88cf7a330fba124488b
juiwenchen <46341773+juiwenchen@users.noreply.github.com>
2024-07-22 15:33:42 +0200

    Update rootfs/scripts/lock.sh
    Co-authored-by: Marco Heinemann <marco.heinemann@useblocks.com>

--- rootfs/scripts/lock.sh
+++ rootfs/scripts/lock.sh
@@ -19,7 +19,6 @@ __ticket_id="$GITHUB_RUN_ID-$(date +%s)-$(( $RANDOM % 1000 ))"
 # $GITHUB_STATE does NOT exist in the post-entry, so $GITHUB_WORKSPACE is used instead to share the info between entry and post-entry
 # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#sending-values-to-the-pre-and-post-actions
 # https://docs.github.com/en/actions/creating-actions/metadata-syntax-for-github-actions#runspre-entrypoint
-# echo "ticket_id=$__ticket_id" >> "$GITHUB_STATE"
 echo "ticket_id=$__ticket_id" > "$GITHUB_WORKSPACE/.$GITHUB_RUN_ID"
 cat "$GITHUB_WORKSPACE/.$GITHUB_RUN_ID"
 
